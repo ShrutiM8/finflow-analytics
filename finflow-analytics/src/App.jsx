f823894062bb9f6223aed19ae2b3062a6fec5e89
Shruti <shrutimukkannawar@gmail.com>
2026-09-23 01:21:33 +0530

    Add dashboard navigation

--- finflow-analytics/src/App.jsx
+++ finflow-analytics/src/App.jsx
@@ -4,10 +4,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
 import Dashboad from './components/Dashboard'
+import Navigation from './components/Navigation'
 function App() {
 
   return (
    <div>
+    <Navigation/>
     <Dashboad/>
    </div> 
   );
---
+++ finflow-analytics/src/components/Navigation.jsx
@@ -0,0 +1,22 @@
+function Navigation() {
+  return (
+    <nav>
+      <div>
+        <span>Logo</span>
+        <h2>Finflow</h2>
+      </div>
+
+      <div>
+        <span>Overview</span>
+        <span>Reports</span>
+        <span>Settings</span>
+      </div>
+
+      <div>
+        <span>Admin User</span>
+      </div>
+    </nav>
+  )
+}
+
+export default Navigation
